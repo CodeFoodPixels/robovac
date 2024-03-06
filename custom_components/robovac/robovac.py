50f7443f736573062602d5cb20efabbf12cce4c6
Luke Bonaccorsi <luke@lukeb.co.uk>
2024-03-06 14:46:02 +0000

    fix: fixes after first test

--- custom_components/robovac/robovac.py
+++ custom_components/robovac/robovac.py
@@ -11,7 +11,7 @@ class RoboVac(TuyaDevice):
     """"""
 
     def __init__(self, model_code, *args, **kwargs):
-        if model_code not in ROBOVAC_MODELS[model_code] is None:
+        if model_code not in ROBOVAC_MODELS:
             raise ModelNotSupportedException(
                 "Model {} is not supported".format(model_code)
             )
@@ -26,20 +26,20 @@ class RoboVac(TuyaDevice):
         return self.model_details.robovac_features
 
     def getFanSpeeds(self):
-        return self.model_details.commands[RobovacCommand.FAN_SPEED].values
+        return self.model_details.commands[RobovacCommand.FAN_SPEED]["values"]
 
     def getModes(self):
-        return self.model_details.commands[RobovacCommand.MODE].values
+        return self.model_details.commands[RobovacCommand.MODE]["values"]
 
     def getSupportedCommands(self):
         return list(self.model_details.commands.keys())
 
     def getCommandCodes(self):
         command_codes = {}
-        for key, value in self.model_details.commands:
+        for key, value in self.model_details.commands.items():
             if isinstance(value, dict):
-                command_codes[key] = value.code
+                command_codes[key] = str(value["code"])
             else:
-                command_codes[key] = value
+                command_codes[key] = str(value)
 
         return command_codes
