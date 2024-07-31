92d4eb7a7140eb189cf62e836129d82534f1a1f8
Maxim Oei <maxim.oei@teampicnic.com>
2024-07-31 16:12:52 +0200

    add T2266

--- custom_components/robovac/vacuums/__init__.py
+++ custom_components/robovac/vacuums/__init__.py
@@ -1,3 +1,4 @@
+from .T1250 import T1250
 from .T2103 import T2103
 from .T2117 import T2117
 from .T2118 import T2118
@@ -7,37 +8,36 @@ from .T2123 import T2123
 from .T2128 import T2128
 from .T2130 import T2130
 from .T2132 import T2132
-from .T1250 import T1250
-from .T2250 import T2250
-from .T2251 import T2251
-from .T2252 import T2252
-from .T2253 import T2253
-from .T2254 import T2254
 from .T2150 import T2150
-from .T2255 import T2255
-from .T2256 import T2256
-from .T2257 import T2257
-from .T2258 import T2258
-from .T2259 import T2259
-from .T2270 import T2270
-from .T2272 import T2272
-from .T2273 import T2273
 from .T2181 import T2181
 from .T2182 import T2182
 from .T2190 import T2190
 from .T2192 import T2192
 from .T2193 import T2193
 from .T2194 import T2194
-from .T2267 import T2267
-from .T2277 import T2277
-from .T2278 import T2278
+from .T2250 import T2250
+from .T2251 import T2251
+from .T2252 import T2252
+from .T2253 import T2253
+from .T2254 import T2254
+from .T2255 import T2255
+from .T2256 import T2256
+from .T2257 import T2257
+from .T2258 import T2258
+from .T2259 import T2259
 from .T2261 import T2261
 from .T2262 import T2262
-from .T2320 import T2320
-from .T2351 import T2351
+from .T2266 import T2266
+from .T2267 import T2267
+from .T2270 import T2270
+from .T2272 import T2272
+from .T2273 import T2273
 from .T2275 import T2275
 from .T2276 import T2276
-
+from .T2277 import T2277
+from .T2278 import T2278
+from .T2320 import T2320
+from .T2351 import T2351
 
 ROBOVAC_MODELS = {
     "T2103": T2103,
@@ -75,6 +75,7 @@ ROBOVAC_MODELS = {
     "T2278": T2278,
     "T2261": T2261,
     "T2262": T2262,
+    "T2266": T2266,
     "T2320": T2320,
     "T2351": T2351,
     "T2275": T2275,
